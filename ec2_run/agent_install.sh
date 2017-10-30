1adfef64f52fef073e71bc0871b5d3bacbdac444
Kris Raney <kraney@ixiacom.com>
2017-10-30 13:50:17 -0700

    Basic script for use with AWS's EC2 Run service.

---
+++ ec2_run/agent_install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# In order for this EC2 Run script to work, the following prerequisites must be met:
+#
+# The EC2 Run agent must be present and functional. This agent is present by default on Amazon Linux, however for
+# it to be functional, be sure to add the AmazonEC2RoleforSSM to your instance role.
+#
+# The SSM Parameter store is used to store the CloudLens API Key in a secure way. Ensure the parameter store is 
+# accessible by the instance's role by adding the AmazonSSMReadOnlyAccess policy to your instance role, or for
+# more restricted access use a policy like this one:
+# 
+#{
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Effect": "Allow",
+#            "Action": [
+#                "ssm:GetParameter"
+#            ],
+#            "Resource": "arn:aws:ssm:*:*:parameter/cloudlens_api_key"
+#        }
+#    ]
+#}
+#
+# You must store the API Key in AWS SSM's parameter store before using this script:
+#
+# aws ssm put-parameter --name cloudlens_api_key --type String --region $REGION --value xxxxxxxxyyyyyyyyzzzzzzzz
+
+
+REGION=us-east-1
+
+# install docker if it's not already present, for popular distros
+if ! which docker 2> /dev/null > /dev/null; then
+    # get.docker.com needs tweaking to support Amazon Linux
+    curl https://get.docker.com/ | sed 's/fedora)/amzn|fedora)/' | sed 's/fedora-26/&\nx86_64-amzn-2017.09/' | bash
+fi
+
+# start docker for rpm-based distros
+sudo systemctl start docker 2> /dev/null || true
+sudo service docker start 2> /dev/null || true
+
+# obtain the API key from SSM
+apikey=$(aws ssm get-parameter --name cloudlens_api_key --region us-east-1 | sudo docker run --rm -i paasmule/curl-ssl-jq jq -r .Parameter.Value)
+
+sudo docker run --name cloudlens-agent \
+                -v /:/host \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -d \
+                --restart=always \
+                --net=host \
+                --device=/dev/mem \
+                --cap-add=SYS_RAWIO \
+                --cap-add=SYS_ADMIN \
+                --cap-add=NET_ADMIN \
+                --cap-add=NET_RAW \
+                ixiacom/cloudlens-agent \
+                --accept_eula y \
+                --apikey "$apikey"
